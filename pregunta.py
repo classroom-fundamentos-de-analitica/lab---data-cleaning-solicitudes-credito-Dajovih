330369c822f7a4ee92fddc6c10c255b6eb9b9e88
Dajovih <jcardonaso@unal.edu.co>
2022-11-27 18:20:56 -0500

    Solución

--- pregunta.py
+++ pregunta.py
@@ -7,14 +7,11 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
 import pandas as pd
-
-
+from datetime import datetime
 def clean_data():
-
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
-
-    #
-    # Inserte su código aquí
-    #
-
+    df['comuna_ciudadano']=df['comuna_ciudadano'].astype(float)
+    df['fecha_de_beneficio']=df['fecha_de_beneficio'].apply(lambda x: datetime.strptime(x, "%Y/%m/%d") if (len(re.findall("^\d+/", x)[0]) - 1) == 4 else datetime.strptime(x, "%d/%m/%Y"))
+    df.dropna(axis = 0, inplace = True)
+    df.drop_duplicates(inplace = True)
     return df
