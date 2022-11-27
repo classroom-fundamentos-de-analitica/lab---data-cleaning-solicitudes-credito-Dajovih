a278fe6fa62a567128da81d4bec9e31f9c6f2f88
Dajovih <jcardonaso@unal.edu.co>
2022-11-27 18:41:46 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -12,17 +12,23 @@ import re
 
 def clean_data():
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
+
     df.dropna(axis = 0, inplace = True)
     df.drop_duplicates(inplace = True)
     df['monto_del_credito']=df['monto_del_credito'].str.replace("\$[\s*]", "")
     df['monto_del_credito']=df['monto_del_credito'].str.replace(",", "")
     df['monto_del_credito']=df['monto_del_credito'].str.replace("\.00", "")
     df['monto_del_credito']=df['monto_del_credito'].astype(int)
+
     df['comuna_ciudadano']=df['comuna_ciudadano'].astype(float)
-    df['fecha_de_beneficio']=df['fecha_de_beneficio'].apply(lambda x: datetime.strptime(x, "%Y/%m/%d") if (len(re.findall("^\d+/", x)[0]) - 1) == 4 else datetime.strptime(x, "%d/%m/%Y"))
+
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(lambda x: datetime.strptime(x, "%Y/%m/%d") if (len(re.findall("^\d+/", x)[0]) - 1) == 4 else datetime.strptime(x, "%d/%m/%Y"))
+
     for i in ['idea_negocio','sexo','barrio','tipo_de_emprendimiento','línea_credito']:
-        df[i] = df[i].str.lower()
-        df[i] = df[i].apply(lambda x: x.replace('_', ' '))
-        df[i] = df[i].apply(lambda x: x.replace('-', ' '))
+        df[i]=df[i].str.lower()
+        df[i]=df[i].apply(lambda x: x.replace('_', ' '))
+        df[i]=df[i].apply(lambda x: x.replace('-', ' '))
+        
     df.drop_duplicates(inplace = True)
+
     return df
