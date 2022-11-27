a8eeed78c1a948618734a7feb941ed57644a08f0
Dajovih <jcardonaso@unal.edu.co>
2022-11-27 18:27:06 -0500

    Actualización

--- pregunta.py
+++ pregunta.py
@@ -10,8 +10,16 @@ import pandas as pd
 from datetime import datetime
 def clean_data():
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df['monto_del_credito']=df['monto_del_credito'].str.replace("\$[\s*]", "")
+    df['monto_del_credito']=df['monto_del_credito'].str.replace(",", "")
+    df['monto_del_credito']=df['monto_del_credito'].str.replace("\.00", "")
+    df['monto_del_credito']=df['monto_del_credito'].astype(int)
     df['comuna_ciudadano']=df['comuna_ciudadano'].astype(float)
     df['fecha_de_beneficio']=df['fecha_de_beneficio'].apply(lambda x: datetime.strptime(x, "%Y/%m/%d") if (len(re.findall("^\d+/", x)[0]) - 1) == 4 else datetime.strptime(x, "%d/%m/%Y"))
+    for i in ['idea_negocio','sexo','barrio','tipo_de_emprendimiento','línea_credito']:
+        df[i] = df[i].str.lower()
+        df[i] = df[i].apply(lambda x: x.replace('_', ' '))
+        df[i] = df[i].apply(lambda x: x.replace('-', ' '))
     df.dropna(axis = 0, inplace = True)
     df.drop_duplicates(inplace = True)
     return df
