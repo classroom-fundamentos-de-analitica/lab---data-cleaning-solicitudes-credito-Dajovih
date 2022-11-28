7ea6b58f5d2d8f4d1a15f8c2027e5222c3ea9ac0
Dajovih <jcardonaso@unal.edu.co>
2022-11-27 20:15:02 -0500

    Update

--- pregunta.py
+++ pregunta.py
@@ -6,29 +6,25 @@ Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizad
 correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
-import pandas as pd
 from datetime import datetime
+import pandas as pd
 import re
 
 def clean_data():
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
-
-    df.dropna(axis = 0, inplace = True)
-    df.drop_duplicates(inplace = True)
-    df['monto_del_credito']=df['monto_del_credito'].str.replace("\$[\s*]", "")
-    df['monto_del_credito']=df['monto_del_credito'].str.replace(",", "")
-    df['monto_del_credito']=df['monto_del_credito'].str.replace("\.00", "")
-    df['monto_del_credito']=df['monto_del_credito'].astype(int)
-
-    df['comuna_ciudadano']=df['comuna_ciudadano'].astype(float)
-
-    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(lambda x: datetime.strptime(x, "%Y/%m/%d") if (len(re.findall("^\d+/", x)[0]) - 1) == 4 else datetime.strptime(x, "%d/%m/%Y"))
-
-    for i in ['idea_negocio','sexo','barrio','tipo_de_emprendimiento','línea_credito']:
-        df[i]=df[i].str.lower()
-        df[i]=df[i].apply(lambda x: x.replace('_', ' '))
-        df[i]=df[i].apply(lambda x: x.replace('-', ' '))
-        
-    df.drop_duplicates(inplace = True)
-
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
+    df.dropna(inplace=True)
+    df.fecha_de_beneficio = [datetime.strptime(i, "%d/%m/%Y") if bool(re.search(r"\d{1,2}/\d{2}/\d{4}", i))
+                             else datetime.strptime(i, "%Y/%m/%d")
+                             for i in df.fecha_de_beneficio]
+    df.barrio = [str.lower(i).replace("_", " ").replace("-", " ") for i in df.barrio]
+    df.sexo = df.sexo.str.lower()
+    df.idea_negocio = [str.lower(i.replace("_", " ").replace("-", " ")) for i in df.idea_negocio]
+    df.tipo_de_emprendimiento = df.tipo_de_emprendimiento.str.lower()
+    df.estrato = df.estrato.astype(int)
+    df.comuna_ciudadano = df.comuna_ciudadano.astype(int)
+    df["línea_credito"] = [str.lower(i.strip().replace("-", " ").replace("_", " ").replace(". ", ".")) for i in
+                           df["línea_credito"]]
+    df.monto_del_credito = [int(i.replace("$ ", "").replace(".00", "").replace(",", "")) for i in
+                            df.monto_del_credito]
+    df.drop_duplicates(inplace=True)
     return df
